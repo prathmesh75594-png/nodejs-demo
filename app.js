7d8037fd5c469fc15941fad03f2c9c1de1a303ec
Prathmesh Pagare <prathmesh75594@gmail.com>
2026-07-19 13:33:20 +0000

    cherry-pick demo commit

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-
-console.log("Hello from main branch!");
 console.log("Hello from FEATURE branch!");
-console.log("this is my second Git commit.");
+console.log("This is my second Git commit.");
 console.log("working on feature branch.");
 
-
-this line is added  on main branch before rebase
+This line is added on main branch before rebase
+This line is added for Git Rebase demo
+Cherry pick demo
